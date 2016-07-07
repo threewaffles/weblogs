5b3b7109af2936b7d3dae086605e435215ff7aaa
ngbmameman <ngbmameman@gmail.com>
2016-07-07 17:11:37 -0400

    start of project

---
+++ main.py
@@ -0,0 +1,18 @@
+#import znc
+from pathlib import Path
+import glob
+
+
+## znc module to view and search logs via the znc control panel.
+
+
+logdir = "C:\\Users\\mameman\\AppData\\Roaming\\HexChat"
+accountname = "Roaming"
+
+def derp(logdir): # lists all log files in the current directory.
+	for filename in Path(logdir).rglob("*.log"):
+		print(filename)
+
+if __name__ == "__main__":
+	derp(logdir)
+
